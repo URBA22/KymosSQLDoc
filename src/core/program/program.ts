7dc535042de4ef6ffd80e12b2b5f22c831403a4c
Simone Urbani <urb.simone@gmail.com>
2023-06-11 16:51:14 +0200

    Add template md file for stored procedure

--- src/core/program/program.ts
+++ src/core/program/program.ts
@@ -17,6 +17,19 @@ export class Program implements IProgram {
     public async executeAsync(argv: string[]): Promise<void> {
         const programOptions = await this.command.parseAsync(argv);
 
+        // 1. prende argomento -s oppure ./ -> source
+        // 2. prende argomento -o oppure ./ -> destination
+
+        // 3. contrtolla che source e destination sia una directory
+
+        // 4. legge la directory source
+        // 5. guarda la cartella restituita
+        // 6. per ogni file nella cartella:
+        //      6.1. esegue il parsing della documentazione
+        //      6.2. crea una cartella speculare in destination
+        //      6.3. crea un file .md con stesso nome e con documentazione
+        // 7. ripete punto 6 per tutte le sotto directory 
+
         console.log(programOptions);
     }
 }

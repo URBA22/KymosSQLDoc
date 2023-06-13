8c8bd36116bb5f0e771cdbe7adcc2e4d1c4cc296
Simone Urbani <urb.simone@gmail.com>
2023-06-13 11:36:44 +0200

    Setup FsManager tests.

---
+++ tests/unit/core/fsmanager.readDirectoryAsync.test.ts
@@ -0,0 +1,24 @@
+
+describe('FsManager ReadDirectoryAsync', () => {
+
+    beforeAll(() => {
+        // TODO: crea durectory con sotto directory e file 
+    });
+
+    test('Shouold read directory when path is absolute', () => {
+        // TODO: Testa passando url assoluto
+    });
+
+    test('Shouold read directory when path is relative', () => {
+        // TODO: Testa passando url relativo
+    });
+
+    test('Shouold throw InvalidPathError when path is not valid', () => {
+        // TODO: Testa passando path non valida
+    });
+
+    afterAll(() => {
+        // TODO: Elimina ttta la directory
+    });
+
+});
---
+++ tests/unit/core/fsmanager.readFileAsync.test.ts
@@ -0,0 +1,24 @@
+
+describe('FsManager ReadFileAsync', () => {
+
+    beforeAll(() => {
+        // TODO: crea file 
+    });
+
+    test('Shouold read file when path is absolute', () => {
+        // TODO: Testa passando url assoluto
+    });
+    
+    test('Shouold read file when path is relative', () => {
+        // TODO: Testa passando url relativo
+    });
+    
+    test('Shouold throw InvalidPathError when path is not valid', () => {
+        // TODO: Testa passando path non valida
+    });
+
+    afterAll(() => {
+        // TODO: Elimina file
+    });
+
+});
